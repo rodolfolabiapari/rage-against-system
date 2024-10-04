740c24810a9195651c745039192247ccb2842a26
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2024-10-04 17:25:48 -0300

    Remocao Keys nao utilizado

--- machine.py
+++ machine.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
